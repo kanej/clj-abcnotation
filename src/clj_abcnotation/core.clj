c70e4adc06459270e5363b8899a12508b6078b78
John Kane <john.kane@spartan-solutions.com>
2013-07-09 21:59:44 +0100

    Added in information fields parsing.

--- src/clj_abcnotation/core.clj
+++ src/clj_abcnotation/core.clj
@@ -2,11 +2,24 @@
   (:require [instaparse.core :as insta]))
 
 (def grammar
-  "S = MOVEMENT (<WHITESPACE?> MOVEMENT)* 
+  "S = INFORMATIONFIELD* MOVEMENTS
+   <INFORMATIONFIELD> = idline | TITLELINE | AUTHORLINE | SOURCELINE | RYTHMNLINE | METERLINE | UNITNOTELENGTHLINE | KEYLINE
+   idline = <'X:'> <WHITESPACE?> DIGIT+ <ENDLINE>
+   TITLELINE = <'T:'> FREETEXT <ENDLINE> 
+   AUTHORLINE = <'Z:'> FREETEXT <ENDLINE> 
+   SOURCELINE = <'S:'> FREETEXT <ENDLINE>
+   RYTHMNLINE = <'R:'> FREETEXT <ENDLINE>
+   METERLINE = <'M:'> FREETEXT <ENDLINE>
+   UNITNOTELENGTHLINE = <'L:'> FREETEXT <ENDLINE>
+   KEYLINE = <'K:'> FREETEXT <ENDLINE>
+
+   MOVEMENTS = MOVEMENT (<WHITESPACE?> MOVEMENT)* 
    MOVEMENT = <'|:'> <WHITESPACE?> BARS <WHITESPACE?> <':|'>
    <BARS> = BAR (<WHITESPACE?>  <'|'> <WHITESPACE?> BAR  )*
    BAR = NOTE (<WHITESPACE?> NOTE)*
    NOTE = LETTER DIGIT?
+   <FREETEXT> = #'[a-zA-Z0-9\\,\\:\\/\\.\\#\\u0020]+'
+   <ENDLINE> = '\\n'
    <LETTER> = #'[a-gA-G]' 
    <DIGIT> = #'[0-9]' 
    <WHITESPACE> = #'\\s+'")
@@ -21,11 +34,22 @@
   ([letter digit] 
      [(letter->note letter) (Integer/parseInt digit)]))
 
+(defn idline->id [id-as-string]
+  [:id (Integer/parseInt id-as-string)])
+
 (def abc-parse (insta/parser grammar))
 
+(defn vec->map [v]
+  (apply hash-map (apply concat v)))
+
 (defn parse [text]
-  (insta/transform {:NOTE note->tuple :S vector}
-    (abc-parse text)))
+  (->> text
+    (abc-parse) 
+    (insta/transform 
+      {:NOTE note->tuple 
+       :S vector
+       :idline idline->id})
+    (vec->map)))
 
 (defn flatten-to-notes [composition]
   (->> composition
--- test/clj_abcnotation/core_test.clj
+++ test/clj_abcnotation/core_test.clj
@@ -2,6 +2,27 @@
   (:require [clojure.test :refer :all]
             [clj-abcnotation.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def butterfly 
+  "X: 1
+T: Butterfly, The
+Z: Jeremy
+S: http://thesession.org/tunes/10#setting10
+R: slip jig
+M: 9/8
+L: 1/8
+K: Emin
+|:B2E G2E F3|B2E G2E FED|B2E G2E F3|B2d d2B AFD:|
+|:B2d e2f g3|B2d g2e dBA|B2d e2f g2a|b2a g2e dBA:|
+|:B3 B2A G2A|B3 BAB dBA|B3 B2A G2A|B2d g2e dBA:|") 
+
+
+(def single-bar "|: def :|")
+
+(deftest parsing-notes
+  (testing "Parse the first bar"
+    (is (= (parse single-bar) {:MOVEMENTS [:MOVEMENT [:BAR [:D5 1] [:E5 1] [:F#5 1]]]} ))))
+
+(deftest parsing-full-piece
+  (testing "Parse the butterfly including its information fields"
+    (let [song (parse butterfly)]
+      (is (= 1 (:id song))))))
